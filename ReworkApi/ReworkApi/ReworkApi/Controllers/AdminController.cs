e818bb5673ee3e1a3c4b84f1dc5254fb8a340db6
jubargra <jubargra@gmail.com>
2024-08-25 23:55:58 -0600

    se agregan metodos en AdminController para completar CRUD tipo rework

--- ReworkApi/ReworkApi/ReworkApi/Controllers/AdminController.cs
+++ ReworkApi/ReworkApi/ReworkApi/Controllers/AdminController.cs
@@ -6,6 +6,7 @@ using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using ReworkApi.Entities;
+using ReworkApi.Models;
 using System.Data;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
@@ -48,7 +49,7 @@ namespace ReworkApi.Controllers
 
         [HttpGet]
         [Route("ConsultarTipoReworks")]
-        public async Task<IActionResult> ConsultarUsuarios()
+        public async Task<IActionResult> ConsultarTipoReworks()
         {
 
             Respuesta resp = new Respuesta();
@@ -67,7 +68,68 @@ namespace ReworkApi.Controllers
                 else
                 {
                     resp.Codigo = 0;
-                    resp.Mensaje = "No hay usuarios registrados en este momento";
+                    resp.Mensaje = "No hay tipos de rework en este momento";
+                    resp.Contenido = false;
+                    return Ok(resp);
+                }
+            }
+        }
+
+        
+        [HttpPut]
+        [Route("ActualizarTipoRework")]
+        public async Task<IActionResult> ActualizarTipoRework(TipoRework ent)
+        {
+    
+            Respuesta resp = new Respuesta();
+
+            using (var context = new SqlConnection(iConfiguration.GetSection("ConnectionStrings:DefaultConnection").Value))
+            {
+                var result = await context.ExecuteAsync("ActualizarTipoRework",
+                    new { ent.id_tipo_rework, ent.descripcion},
+                    commandType: System.Data.CommandType.StoredProcedure);
+
+                if (result > 0)
+                {
+                    resp.Codigo = 1;
+                    resp.Mensaje = "OK";
+                    resp.Contenido = true;
+                    return Ok(resp);
+                }
+                else
+                {
+                    resp.Codigo = 0;
+                    resp.Mensaje = "La información del tipo rework no se pudo actualizar";
+                    resp.Contenido = false;
+                    return Ok(resp);
+                }
+            }
+        }
+
+        [HttpDelete]
+        [Route("EliminarTipoRework")]
+        public async Task<IActionResult> EliminarTipoRework(TipoRework ent)
+        {
+
+            Respuesta resp = new Respuesta();
+
+            using (var context = new SqlConnection(iConfiguration.GetSection("ConnectionStrings:DefaultConnection").Value))
+            {
+                var result = await context.ExecuteAsync("EliminarTipoRework",
+                    new { ent.id_tipo_rework},
+                    commandType: System.Data.CommandType.StoredProcedure);
+
+                if (result > 0)
+                {
+                    resp.Codigo = 1;
+                    resp.Mensaje = "OK";
+                    resp.Contenido = true;
+                    return Ok(resp);
+                }
+                else
+                {
+                    resp.Codigo = 0;
+                    resp.Mensaje = "No se pudo eliminar el tipo rework seleccionado";
                     resp.Contenido = false;
                     return Ok(resp);
                 }
